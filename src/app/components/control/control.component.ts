1fc10c6f88048fad3faf2456c07954fe24438566
aruidev <arucadev@gmail.com>
2025-11-09 20:37:21 +0100

    feat: add lucide-angular icons and update button styles; remove unused SVG assets

--- src/app/components/control/control.component.ts
+++ src/app/components/control/control.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, PauseIcon, Play, Square } from 'lucide-angular';
 
 @Component({
   selector: 'app-control',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
 })
 export class ControlComponent {
-
+  readonly PlayIcon = Play;
+  readonly PauseIcon = PauseIcon;
+  readonly StopIcon = Square;
 }
