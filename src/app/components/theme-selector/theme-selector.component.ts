e870c348e471cf52965f267908c4169b9df286c9
aruidev <arucadev@gmail.com>
2025-11-09 20:18:09 +0100

    chore: update Angular dependencies to version 20.3.9
    style: update footer text color to use CSS variable
    
    style: enhance theme selector styles and structure
    
    feat: implement theme selection with local storage support
    
    fix: wrap app-cup component in a div for layout consistency
    
    fix: bind data-theme attribute to html element for theme management
    
    style: refine CSS variables for themes and add transition effects
    
    feat: add SVG assets for cup, play, pause, and stop icons

--- src/app/components/theme-selector/theme-selector.component.ts
+++ src/app/components/theme-selector/theme-selector.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme-selector',
-  imports: [NgbAccordionModule],
+  imports: [],
   templateUrl: './theme-selector.component.html',
   styleUrl: './theme-selector.component.css',
 })
-export class ThemeSelectorComponent {
-  selectedTheme = ''; // o 'tea' por defecto
+export class ThemeSelectorComponent implements OnInit {
+
+  selectedTheme = 'tea';
+
+  ngOnInit(): void {
+    const theme = this.getLocalTheme();
+    this.setTheme(theme);
+  }
+
+  getTheme() {
+    return this.selectedTheme;
+  }
+
+  getLocalTheme() {
+    const storedTheme = localStorage.getItem('theme');
+    return storedTheme ? storedTheme : 'tea';
+  }
 
   setTheme(themeName: string) {
     this.selectedTheme = themeName;
     document.documentElement.setAttribute('data-theme', themeName);
+    localStorage.setItem('theme', themeName);
   }
 }
